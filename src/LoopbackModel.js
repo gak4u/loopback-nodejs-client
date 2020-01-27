dc4b012983cdd43d3c9df93962616ae2d1c5b000
Anil Kumar Gurram <anil.virtuali@gmail.com>
2020-01-28 02:33:42 +0530

    Update LoopbackModel.js

--- src/LoopbackModel.js
+++ src/LoopbackModel.js
@@ -9,7 +9,7 @@ const qs = require("qs");
 const handleErrors = response => {
   if (!response.ok) {
     if(response.status == 422){
-      respone.json().then(res => {throw res});
+      response.json().then(res => {throw res});
     }else{
       throw Error(response.statusText);
     }
